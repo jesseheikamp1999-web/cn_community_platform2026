b99f5ddb02efa513f8daddc91d343fa8a3904b1e
jesseheikamp1999-web <jesseheikamp1999@gmail.com>
2026-06-19 13:24:45 +0200

    Fix public avatars and pagination sizing

--- resources/views/components/user-avatar.blade.php
+++ resources/views/components/user-avatar.blade.php
@@ -2,9 +2,9 @@
 
 @php($initials = strtoupper(mb_substr($user->name, 0, 2)))
 
-<span class="user-avatar-wrap {{ $class }}">
+<span class="user-avatar-wrap {{ $class }}" aria-label="{{ $user->name }}">
     @if($user->discord_avatar_url)
-        <img class="user-avatar-image" src="{{ $user->discord_avatar_url }}" alt="" referrerpolicy="no-referrer" onerror="this.hidden=true; this.nextElementSibling.hidden=false">
+        <img class="user-avatar-image" src="{{ $user->discord_avatar_url }}" alt="" loading="lazy" decoding="async" referrerpolicy="no-referrer" onerror="this.hidden=true; this.nextElementSibling.hidden=false">
         <span class="user-avatar-fallback" hidden>{{ $initials }}</span>
     @else
         <span class="user-avatar-fallback">{{ $initials }}</span>
